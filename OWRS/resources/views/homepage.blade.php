c431cbd4eb6419a13943f3f02a6019b39a1238dd
Nucleus04 <98724850+Nucleus04@users.noreply.github.com>
2023-02-20 06:33:38 +0800

    home header

--- OWRS/resources/views/homepage.blade.php
+++ OWRS/resources/views/homepage.blade.php
@@ -8,14 +8,28 @@
     @vite('resources/css/app.css')
 </head>
 <body>
-    <div class="w-full bg-green-500 h-screen">
-        <div class="md:h-12 bg-red-500 md:block hidden">
+    <div class="w-full h-screen">
+        <div class="h-12 bg-sky-400">
             <div class=" h-full w-full grid grid-cols-5">
-                <a href="#" class="text-center my-auto">Home</a>
-                <a href="#" class="text-center my-auto">Place an Order</a>
-                <p class="text-center my-auto">Youhave currently Order</p>
-                <a href="#" class="text-center my-auto">Transaction History</a>
-                <a href="#" class="text-center my-auto">Profile</a>
+                <a href="#" class="my-auto mx-auto flex flex-cols-2">
+                    <img src="{{url('/assets/home.png')}}" class="w-8 h-8  mr-1" alt="">
+                    <p class="text-center my-auto hidden sm:block">Home</p>
+                </a>
+                <a href="#" class="my-auto mx-auto flex flex-cols-2">
+                    <img src="{{url('/assets/placeorder.png')}}" class="w-8 h-8  mr-1" alt="">
+                    <p class="text-center my-auto hidden sm:block">Place Order</p>
+                </a>
+                <div class="my-auto mx-auto bg-silver lg:w-48 md:w-32 w-24 rounded-xl">
+                    <p class="text-center my-auto">Active Order</p>
+                </div>
+                <a href="#" class="my-auto mx-auto flex flex-cols-2">
+                    <img src="{{url('/assets/history.png')}}" class="w-8 h-8 mr-1" alt="">
+                    <p class="text-center my-auto hidden sm:block">History</p>
+                </a>
+                <a href="#" class="my-auto mx-auto flex flex-cols-2">
+                    <img src="{{url('/assets/profile.png')}}" class="w-8 h-8  mr-1" alt="">
+                    <p class="text-center my-auto hidden sm:block">Profile</p>
+                </a>
             </div>
         </div>
     </div>
