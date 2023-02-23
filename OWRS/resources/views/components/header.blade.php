400fc3df5543789bdeb6db311f83af145f8a6271
Maxnuggets <jericholongabela@gmail.com>
2023-02-23 16:30:08 +0800

    edited header

--- OWRS/resources/views/components/header.blade.php
+++ OWRS/resources/views/components/header.blade.php
@@ -3,24 +3,24 @@
         <div class=" h-full w-full grid grid-cols-5">
             <a href="/home" class="my-auto mx-auto flex flex-cols-2 hover:bg-sky-500 rounded-full sm:px-2 sm:py-1 p-1 hover:border-2 border-sky-600">
                 <img src="{{url('/assets/home.png')}}" class="w-6 h-6 sm:mr-1 " alt="">
-                <p class="text-center my-auto hidden sm:block">Home</p>
+                <b><p class="text-center my-auto hidden sm:block">Home</p></b>
             </a>
             <a href="/place-order" class="my-auto mx-auto flex flex-cols-2 hover:bg-sky-500 rounded-full sm:px-2 sm:py-1 p-1 hover:border-2 border-sky-600">
                 <img src="{{url('/assets/placeorder.png')}}" class="w-6 h-6  sm:mr-1" alt="">
-                <p class="text-center my-auto hidden sm:block">Place Order</p>
+                <b><p class="text-center my-auto hidden sm:block">Place Order</p></b>
             </a>
             <a href="/active-order" class="my-auto mx-auto">
-                <div class=" bg-silver lg:w-56 md:w-32 w-24 rounded-xl shadow-xl hover:-translate-y-0.5">
-                    <p class="text-center my-auto md:text-lg sm:text-md text-sm">Active Order</p>
+                <div class="flex justify-center bg-silver lg:w-56 h-8 md:w-32 w-24 rounded-md shadow-xl hover:-translate-y-0.5">
+                    <p class="text-center my-auto md:text-md sm:text-s text-xs">Order to be delivered before <b>2:45 pm</b></p>
                 </div>
             </a>
             <a href="#" class="my-auto mx-auto flex flex-cols-2 hover:bg-sky-500 rounded-full sm:px-2 sm:py-1 p-1 hover:border-2 border-sky-600">
                 <img src="{{url('/assets/history.png')}}" class="w-6 h-6 sm:mr-1" alt="">
-                <p class="text-center my-auto hidden sm:block">History</p>
+                <b><p class="text-center my-auto hidden sm:block">History</p></b>
             </a>
             <a href="/profile" class="my-auto mx-auto flex flex-cols-2 hover:bg-sky-500 rounded-full sm:px-2 sm:py-1 p-1 hover:border-2 border-sky-600">
                 <img src="{{url('/assets/profile.png')}}" class="w-6 h-6 sm:mr-1" alt="">
-                <p class="text-center my-auto hidden sm:block">Profile</p>
+                <b><p class="text-center my-auto hidden sm:block">Profile</p></b>
             </a>
         </div>
     </div>
